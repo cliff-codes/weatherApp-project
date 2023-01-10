cf731fe05c0c31bd5e4bda370bf481d124286a82
Clifford <=>
2023-01-10 08:44:32 +0000

    modified forcast

--- app.js
+++ app.js
@@ -1,15 +1,16 @@
 // //Making http request using the request package from npm
-const request = require('request')
+// const request = require('request')
+
 const geoCode = require('./utils/geocode')
 const forcast = require('./utils/forcast')
 
 forcast('Drobo',(err,data) => {
      console.log('Error : ', err)
-     console.log('Data : ', data)
+     console.log('Data : ', data.current.temperature)
 })
 
-// geoCode('Kumasi',(err,data) => {
-//      console.log('Error',err)
-//      console.log('Data',data)
-// })
+geoCode('Kumasi',(err,data) => {
+     console.log('Error',err)
+     console.log('Data',data)
+})
 
